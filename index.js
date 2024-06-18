8f5a663ab80dc21bc4dc7203893d0c4fe67327a9
kasimxo <kasimxo@gmail.com>
2024-06-18 10:26:55 +0200

    Test: Request + answer

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+import http from 'node:http'
+
+http.createServer(function (req, res) {
+    res.write('Hello World!'); //write a response to the client
+    console.log('Ha llegado una petición')
+    res.end(); //end the response
+}).listen(8080)
+
 console.log("test")
 console.log("prueba")
